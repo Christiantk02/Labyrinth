29fe30f1457b3d599857c541a927be72909cb419
Aron Larsen <arondl@stud.ntnu.no>
2025-05-06 12:34:07 +0200

    Colour calibration og A*(Utestet)

--- ColourCalibrate.py
+++ ColourCalibrate.py
@@ -2,14 +2,41 @@ import cv2
 import cvzone
 from cvzone.ColorModule import ColorFinder
 
+#Funksjon for å liste opp tilgjengelige kamera-porter
+def availableCameras(maxPorts = 5):
+    availablePorts = []
+    for i in range(maxPorts):
+        cap = cv2.VideoCapture(i)
+        if cap.isOpened():
+            availablePorts.append(i)
+        cap.release()
+    return availablePorts
+
+#Viser hvilken kamera som er tilgjengelige
+print("Søker etter tilgjengelige kamera")
+availablePorts = availableCameras()
+
+if not availablePorts:
+    print("Ingen tilgjengelige kamera detektert")
+    exit()
+
+print(f"Tilgjengelige kamera-porter: {availablePorts}")
+
+#Velge kamera-port manuelt
+CAMERA_PORT = -1
+RESOLUTION = (640, 480)
+
+while CAMERA_PORT not in availablePorts:
+    CAMERA_PORT = int(input(f"Velg et tilgjengelig kamera fra denne lista ({availablePorts}):"))
+
 # Kamera port (prøv 0, 1, eller 2)
-CAMERA_PORT = 1  
+#CAMERA_PORT = 1  
 
 # Opprett ColorFinder i kalibreringsmodus
 myColorFinder = ColorFinder(True)  # Sett til True for å kunne justere HSV
 
 # Åpne kamera
-camera = cv2.VideoCapture(CAMERA_PORT, cv2.CAP_DSHOW)
+camera = cv2.VideoCapture(CAMERA_PORT)
 if not camera.isOpened():
     print(f"Feil: Kunne ikke åpne kamera på port {CAMERA_PORT}")
     exit()
